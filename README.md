880403dd77b12b9f57e9a6315f5742dc9c63ca58
Matt Mower <self@mattmower.com>
2014-09-26 09:34:41 +0100

    Rules are now defined in a separate edn file and dynamically loaded. Rules can be overridden. Better README.

--- README.md
+++ README.md
@@ -5,22 +5,28 @@ If you've ever wanted to create a nonsense English dictionary then Wordmash is f
 To create a new word, Wordmash takes two randomly selected words from the original dictionary, selects a random mid-point for each word, and
 splits them to form two beginnings and two endings. It then takes the first beginning and second ending and makes a new word. Here's an example:
 
-* Randomly choosen words "unbalance" and "guayacan"
+1. Randomly choosen words "unbalance" and "guayacan"
 * For "unbalance" select random mid-point, e.g. 3
 * Split "unbalance" at 3 to form "unb" and "alance"
 * For "guayacan" select random mid-point, e.g. 4
 * Split "guayacan" at 4 to form "guay" and "acan"
 * Combine "unb" with "acan" to form new word "unbacan"
 
+Wordmash includes some simple rules that try to ensure generated words seem English-like for example not allowing endings like i, u, v, or j, or allowing words with a q not followed by a u. The default rules are defined in `resources/rules.edn`.
+
 ## Usage
 
-lein uberjar
-java -jar target/wordmash-0.1.1-SNAPSHOT-standalone.jar <output-dictionary> <word-count>
+    lein uberjar
+    java -jar target/wordmash-0.1.2-SNAPSHOT-standalone.jar <output-dictionary> <word-count>
 
-by default it will look in /usr/share/dict/words, to use a different dictionary pass
-the path in DICTFILE environment
+by default it will look in `/usr/share/dict/words`, to use a different dictionary pass
+the path in `DICTFILE` environment:
 
-DICTFILE=/my/special/dict/words java -jar ...
+    DICTFILE=/my/special/dict/words java -jar ...
+
+to use a custom rules files pass the path to your own edn file using the `RULESFILE` environment variable:
+
+    RULESFILE=/my/rules.edn java -jar ...
 
 ## License
 
--- src/wordmash/core.clj
+++ src/wordmash/core.clj
@@ -1,11 +1,22 @@
 (ns wordmash.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn])
   (:gen-class :main true))
+
+(defn rules-file
+  []
+  (or (System/getenv "RULESFILE")
+      "resources/rules.edn"))
+
+(defn dictionary-file
+  []
+  (or (System/getenv "DICTFILE")
+      "/usr/share/dict/words"))
 
 (defn read-dictionary
   "Return a sequence of dictionary words between 4 and 8 characters long and containing no upper case letters."
-  [dictionary-file]
-  (->> (slurp dictionary-file)
+  [dict-file]
+  (->> (slurp dict-file)
        (str/split-lines)
        (filter #(> 9 (count %) 3))
        (filter #(= % (str/lower-case %)))))
@@ -33,13 +44,11 @@
         [_ e2] (split-word (random-word dictionary))]
     (str/join [b1 e2])))
 
-(def invalid-letter-patterns {:triple-consonant "([b-df-hj-np-tv-z]){3}"
-                              :duplicate-vowels "([aiu])\1{1,}"
-                              :too-many-voewls "([aeiou]){3,}"
-                              :bad-ending "[iuvj]\b"
-                              :q-without-u "q[^u]"})
+(defn rule-set
+  []
+  (vals (edn/read-string (slurp (rules-file)))))
 
-(def invalid-word-regex (re-pattern (apply str (interpose "|" (vals invalid-letter-patterns)))))
+(def invalid-word-regex (re-pattern (apply str (interpose "|" (rule-set)))))
 
 (defn viable-word?
   "Predicate that ensures the word w contains none of the invalid letter patterns."
@@ -59,11 +68,6 @@
 (defn create-mash-dictionary
   [mash-file dictionary size]
   (spit mash-file (str/join "\n" (sort (take size (viable-word-stream dictionary))))))
-
-(defn dictionary-file
-  []
-  (or (System/getenv "DICTFILE")
-      "/usr/share/dict/words"))
 
 (defn -main
   [output-file size & args]
